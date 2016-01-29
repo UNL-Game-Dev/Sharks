86b357c328e2e3648545aff0d0445e6f6aa7d050
Sam Adams <samadams532@gmail.com>
2016-01-29 16:45:15 -0600

    Turning is harder when you are going slowly

--- Sharks/Assets/Scripts/TopDownMovement.cs
+++ Sharks/Assets/Scripts/TopDownMovement.cs
@@ -13,7 +13,8 @@ public class TopDownMovement : MonoBehaviour {
 
 	void FixedUpdate() {
 		float aimingInput = Input.GetAxis ("Horizontal");
-		GetComponent<Rigidbody2D>().angularVelocity = -rotationSpeed * aimingInput;
+		var currentSpeed = GetComponent<Rigidbody2D> ().velocity.magnitude;
+		GetComponent<Rigidbody2D>().angularVelocity = -rotationSpeed * aimingInput * currentSpeed / speed;
 
 		float input = Input.GetAxis ("Vertical");
 		GetComponent<Rigidbody2D>().AddForce (gameObject.transform.up * speed * input);
