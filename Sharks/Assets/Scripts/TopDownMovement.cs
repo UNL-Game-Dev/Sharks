635541dc5b1a5e539e86bf96a39274bc5b06fe4f
Sam Adams <samadams532@gmail.com>
2016-01-29 16:34:24 -0600

    re-implemented everything

--- Sharks/Assets/Scripts/TopDownMovement.cs
+++ Sharks/Assets/Scripts/TopDownMovement.cs
@@ -3,13 +3,29 @@ using System.Collections;
 
 public class TopDownMovement : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+	public float speed;
+
+	public float rotationSpeed;
+
+	public float barrierHardness;
+
+	public float arenaRadius;
+
+	void FixedUpdate() {
+		float aimingInput = Input.GetAxis ("Horizontal");
+		GetComponent<Rigidbody2D>().angularVelocity = -rotationSpeed * aimingInput;
+
+		float input = Input.GetAxis ("Vertical");
+		GetComponent<Rigidbody2D>().AddForce (gameObject.transform.up * speed * input);
+
+		EnforceBoundaries ();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	private void EnforceBoundaries() {
+		var distanceFromCenter = gameObject.transform.position.magnitude;
+
+		if (distanceFromCenter > arenaRadius) {
+			GetComponent<Rigidbody2D> ().AddForce (-gameObject.transform.position.normalized * barrierHardness * (distanceFromCenter - arenaRadius));
+		}
 	}
 }
