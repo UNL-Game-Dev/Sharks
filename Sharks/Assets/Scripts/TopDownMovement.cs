488d15a3eb2212fc8331a35b614d10d205abca14
Sam Adams <samadams532@gmail.com>
2016-01-29 15:48:11 -0600

    Added movement script with mouse aiming

--- Sharks/Assets/Scripts/TopDownMovement.cs
+++ Sharks/Assets/Scripts/TopDownMovement.cs
@@ -3,13 +3,17 @@ using System.Collections;
 
 public class TopDownMovement : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+	public float speed;
+
+	void FixedUpdate() {
+		var mousePosition = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+		Quaternion rot = Quaternion.LookRotation (transform.position - mousePosition,
+			                 Vector3.forward);
+		transform.rotation = rot;
+		transform.eulerAngles = new Vector3 (0, 0, transform.eulerAngles.z);
+		GetComponent<Rigidbody2D>().angularVelocity = 0;
+
+		float input = Input.GetAxis ("Vertical");
+		GetComponent<Rigidbody2D>().AddForce (gameObject.transform.up * speed * input);
 	}
 }
