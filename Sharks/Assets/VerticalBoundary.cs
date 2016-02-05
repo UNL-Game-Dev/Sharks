6f506cf147d003fb75132514e847c4bcd148bc51
Sam Adams <samadams532@gmail.com>
2016-02-05 16:34:03 -0600

    Put moving hazard in there.

---
+++ Sharks/Assets/VerticalBoundary.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VerticalBoundary : MonoBehaviour {
+	
+	void Start () {
+		
+	}
+
+	void FixedUpdate () {
+		var period = 3.0f;
+		var a = new Vector3 (0.0f, 2.5f, 0.0f);
+		var b = new Vector3 (0.0f, 22.5f, 0.0f);
+		var time = Time.timeSinceLevelLoad;
+		var t = time % period;
+		if (t < period / 2) {
+			transform.position = a + (b - a) * t / (period / 2);
+		} else {
+			transform.position = b + (b - a) * (period / 2 - t) / (period / 2);
+		}
+	}
+}
