545c540f1938cc85f1dfac2792a092a905fc807f
Sam Adams <samadams532@gmail.com>
2016-02-05 15:58:33 -0600

    Added ball you can push around, also responds to same boundaries

--- Sharks/Assets/Scripts/TopDownMovement.cs
+++ Sharks/Assets/Scripts/TopDownMovement.cs
@@ -7,10 +7,6 @@ public class TopDownMovement : MonoBehaviour {
 
 	public float rotationSpeed;
 
-	public float barrierHardness;
-
-	public float arenaRadius;
-
 	void FixedUpdate() {
 		float aimingInput = Input.GetAxis ("Horizontal");
 		var currentSpeed = GetComponent<Rigidbody2D> ().velocity.magnitude;
@@ -18,15 +14,5 @@ public class TopDownMovement : MonoBehaviour {
 
 		float input = Input.GetAxis ("Vertical");
 		GetComponent<Rigidbody2D>().AddForce (gameObject.transform.up * speed * input);
-
-		EnforceBoundaries ();
-	}
-
-	private void EnforceBoundaries() {
-		var distanceFromCenter = gameObject.transform.position.magnitude;
-
-		if (distanceFromCenter > arenaRadius) {
-			GetComponent<Rigidbody2D> ().AddForce (-gameObject.transform.position.normalized * barrierHardness * (distanceFromCenter - arenaRadius));
-		}
 	}
 }
---
+++ Sharks/Assets/Scripts/TrackBoundary.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrackBoundary : MonoBehaviour {
+
+	public float barrierHardness;
+
+	public float outerRadius;
+
+	public float innerRadius;
+
+	void FixedUpdate() {
+		var distanceFromCenter = gameObject.transform.position.magnitude;
+
+		var rigidBody = GetComponent<Rigidbody2D> ();
+		if (distanceFromCenter > outerRadius) {
+			rigidBody.AddForce (-gameObject.transform.position.normalized * barrierHardness * (distanceFromCenter - outerRadius));
+		}
+
+		if (distanceFromCenter < innerRadius) {
+			rigidBody.AddForce (-gameObject.transform.position.normalized * barrierHardness * (innerRadius - distanceFromCenter));
+		}
+	}
+}
