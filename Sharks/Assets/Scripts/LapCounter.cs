d53d747b7ff2cbb25acae3ab9dbf7bf56bdc9d75
Sam Adams <samadams532@gmail.com>
2016-02-12 15:17:14 -0600

    Beginnings of lap counter

---
+++ Sharks/Assets/Scripts/LapCounter.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LapCounter : MonoBehaviour {
+
+	private int currentLap = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void AddLap() {
+		currentLap++;
+		print ("Lap " + currentLap + "!");
+	}
+}
