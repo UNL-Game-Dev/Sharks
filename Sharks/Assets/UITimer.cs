39488d727aeab46c65003e6e35fad03ec41c4a6c
mattkachek <mkachek@icloud.com>
2016-02-12 14:40:35 -0600

    Added a simple UI with a timer
    The UI is the canvas layer and just contains a timer text and a script
    to run a timer and display it there.

---
+++ Sharks/Assets/UITimer.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UITimer : MonoBehaviour
+{
+
+    private float timer = 0.0f;
+    private Text theText;
+    private Component comp;
+
+    // Use this for initialization
+    void Start()
+    {
+        comp = this.gameObject.GetComponent("Text");
+        theText = comp.GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        theText.text = "Time: " + timer.ToString("F2");
+    }
+}
