8a8523e02cda7be5812a3fe22db46a3111f928c8
Sam Adams <samadams532@gmail.com>
2016-02-12 15:43:57 -0600

    Prevent lap boundary from working backwards

--- Sharks/Assets/Scripts/FinishLine.cs
+++ Sharks/Assets/Scripts/FinishLine.cs
@@ -14,9 +14,15 @@ public class FinishLine : MonoBehaviour {
 	}
 
 	void OnTriggerEnter2D(Collider2D other) {
-		var lapCounter = other.GetComponent<LapCounter> ();
-		if (lapCounter != null) {
-			lapCounter.AddLap ();
+		// Math.
+		var trackVelocity = Vector3.Dot(Vector3.Cross(other.transform.position, other.GetComponent<Rigidbody2D>().velocity), new Vector3(0, 0, 1));
+
+		// Only lap if going in the right direction.
+		if (trackVelocity > 0) {
+			var lapCounter = other.GetComponent<LapCounter> ();
+			if (lapCounter != null) {
+				lapCounter.AddLap ();
+			}
 		}
 	}
 }
