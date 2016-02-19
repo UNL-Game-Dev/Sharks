cb2afd7f91a17894dbfd0d26e58eff46d9e027a2
Sam Adams <samadams532@gmail.com>
2016-02-19 15:25:39 -0600

    put solid "island" in center

--- Sharks/Assets/Scripts/TrackBoundary.cs
+++ Sharks/Assets/Scripts/TrackBoundary.cs
@@ -7,18 +7,12 @@ public class TrackBoundary : MonoBehaviour {
 
 	public float outerRadius;
 
-	public float innerRadius;
-
 	void FixedUpdate() {
 		var distanceFromCenter = gameObject.transform.position.magnitude;
 
 		var rigidBody = GetComponent<Rigidbody2D> ();
 		if (distanceFromCenter > outerRadius) {
 			rigidBody.AddForce (gameObject.transform.position.normalized * barrierHardness * (outerRadius - distanceFromCenter));
-		}
-
-		if (distanceFromCenter < innerRadius) {
-			rigidBody.AddForce (gameObject.transform.position.normalized * barrierHardness * (innerRadius - distanceFromCenter));
 		}
 	}
 }
