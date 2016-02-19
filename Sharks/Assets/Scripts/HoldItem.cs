6f3f135939b92beffa5022efa6aec9afaef3b639
Sam Adams <samadams532@gmail.com>
2016-02-19 16:04:11 -0600

    Listening to Inception and writing this commit message

---
+++ Sharks/Assets/Scripts/HoldItem.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HoldItem : MonoBehaviour {
+
+	private string item;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			DeployItem ();
+		}
+	}
+
+	void TakeItem(string item) {
+		if (this.item == null) {
+			this.item = item;
+		}
+	}
+
+	void DeployItem() {
+		switch (this.item) {
+		case "laser":
+			// TODO: Create a laser anchor and laser object
+			break;
+		default:
+			break;
+		}
+		this.item = null;
+	}
+}
