691cd7b3ef2a374b15cf55f80bff095c907c3691
SamGerdes <segerdes@cox.net>
2016-02-26 12:31:28 -0600

    Pickup script

---
+++ Sharks/Assets/Scripts/Pickup.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pickup : MonoBehaviour
+{
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            other.SendMessage("TakeItem", "laser");
+            Destroy(gameObject);
+        }
+
+    }
+}
