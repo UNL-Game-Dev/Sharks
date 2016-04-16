44055ac8597a72ac5be610507946673e1573b13d
SamGerdes <segerdes@cox.net>
2016-04-15 20:59:22 -0500

    Powerup spawning and disappearing added

---
+++ Sharks/Assets/Scripts/PowerupSpawn.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PowerupSpawn : MonoBehaviour
+{
+    public Rigidbody2D Pickup;
+    public float spawnTime = 3f;
+
+
+	void Start () {
+        InvokeRepeating("Spawn", spawnTime, spawnTime);
+	}
+
+
+    void Spawn()
+    {
+        var angle = Random.Range(0, 2 * Mathf.PI);
+        Rigidbody2D Projectile = (Rigidbody2D)Instantiate(Pickup, transform.position, transform.rotation);
+        Projectile.velocity = new Vector3(Mathf.Cos(angle), Mathf.Sin(angle), 0) * 10;
+	}
+    
+	
+	
+}
