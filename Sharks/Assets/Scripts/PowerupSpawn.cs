ad47d4883c08bd5e04350a5c0e5b04f499ec344d
SamGerdes <segerdes@cox.net>
2016-04-22 15:16:00 -0500

    Spawning multiple powerups

--- Sharks/Assets/Scripts/PowerupSpawn.cs
+++ Sharks/Assets/Scripts/PowerupSpawn.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class PowerupSpawn : MonoBehaviour
 {
-    public Rigidbody2D Pickup;
+    public Rigidbody2D[] Pickups;
     public float spawnTime = 3f;
 
 
@@ -15,6 +15,7 @@ public class PowerupSpawn : MonoBehaviour
     void Spawn()
     {
         var angle = Random.Range(0, 2 * Mathf.PI);
+        var Pickup = Pickups[Random.Range(0, Pickups.Length)];
         Rigidbody2D Projectile = (Rigidbody2D)Instantiate(Pickup, transform.position, transform.rotation);
         Projectile.velocity = new Vector3(Mathf.Cos(angle), Mathf.Sin(angle), 0) * 10;
 	}
